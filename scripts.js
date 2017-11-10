fd8369e15eeef60d08ca7768d5ce74d0aaa92c41
Ophus Wong <ophus.wong@gmail.com>
2017-11-10 10:55:20 -0700

    Add toggle for chevron

--- scripts.js
+++ scripts.js
@@ -4,4 +4,5 @@ $('.side-nav-reveal').on('click', function() {
 
 $('.stat-chevron').on('click', function() {
 	$('.stat-dropdown').slideToggle(300);
+	$(this).find('i').toggleClass('fa fa-chevron-up fa fa-chevron-down')
 });
